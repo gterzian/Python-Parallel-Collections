e7a1ed85c34d1b375310d390849edfe5200e92e4
Gregory <gregory.terzian@gmail.com>
2015-03-17 19:41:48 +0800

    bumped up the version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import parallel
 setup(
     name='python-parallel-collections',
     data_files=[('', ['requirements.txt', 'README.md', '.gitignore']),],
-    version='1.2',
+    version='1.2.1',
     packages=['parallel',],
     description='parallel support for map/reduce style operations',
     long_description='''

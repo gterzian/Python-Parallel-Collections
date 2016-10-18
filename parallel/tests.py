da7865adbf75b87d969cf9a73d6b1ebb79d8cbc1
Gregory Terzian <gterzian@users.noreply.github.com>
2016-10-18 23:08:17 +0800

    map first, then flatten
    first map, then flatten
    
    try to fix tests
    
    now really fixing tests
    
    using chain instead of generator
    
    fix call to chain
    
    fix chaining
    
    fix chain
    
    fix chaining, bis
    
    fix tests

--- parallel/tests.py
+++ parallel/tests.py
@@ -10,49 +10,44 @@ from parallel_collections import (
 
 
 class TestHelpers(unittest.TestCase):
-    
+
     def test_reducer_two_items(self):
         reducer = _Reducer(group_letters, defaultdict(list))
         reducer('a')
         reducer('a')
         self.assertEquals(reducer.result, {'a': ['a', 'a']})
-        
+
     def test_reducer_three_items(self):
         reducer = _Reducer(group_letters, defaultdict(list))
         reducer('a')
         reducer('a')
         reducer('b')
         self.assertEquals(reducer.result, {'a': ['a', 'a'], 'b': ['b']})
-        
+
     def test_filter_none(self):
         _filter = _Filter(is_digit)
         self.assertEquals(_filter('a'), (False, None))
-    
+
     def test_filter_returns_passing_item(self):
         _filter = _Filter(is_digit)
         self.assertEquals(_filter('1'), (True, '1'))
-        
+
 class TestGen(unittest.TestCase):
-    
-    def test_flatten(self):
-        p = parallel((d for d in [range(10),range(10)]))
-        self.assertTrue(p.__class__.__name__ == 'ParallelGen')
-        self.assertEquals(list(p.flatten()), list(chain(*[range(10),range(10)])))
-        
+
     def test_foreach(self):
         p = parallel((d for d in [range(10),range(10)]))
         self.assertTrue(p.__class__.__name__ == 'ParallelGen')
         p.foreach(double)
         self.assertEquals(list(p), map(double, [range(10),range(10)]))
         self.assertTrue(p.foreach(double) is None)
-        
+
     def test_map(self):
         p = parallel((d for d in [range(10),range(10)]))
         self.assertTrue(p.__class__.__name__ == 'ParallelGen')
         mapped = p.map(double)
         self.assertEquals(list(mapped), map(double, [range(10),range(10)]))
         self.assertFalse(mapped is p)
-    
+
     def test_filter(self):
         p = parallel((d for d in ['a','2','3']))
         self.assertTrue(p.__class__.__name__ == 'ParallelGen')
@@ -60,7 +55,7 @@ class TestGen(unittest.TestCase):
         filtered = p.filter(pred)
         self.assertEquals(list(filtered), list(['2','3']))
         self.assertFalse(filtered is p)
-        
+
     def test_filter_with_ret_none_func(self):
         p = parallel((d for d in [True, False]))
         self.assertTrue(p.__class__.__name__ == 'ParallelGen')
@@ -86,31 +81,23 @@ class TestGen(unittest.TestCase):
         self.assertFalse(filtered is p)
 
     def test_flatmap(self):
-        p = parallel((d for d in [range(10),range(10)]))
+        p = parallel([range(10),range(10)])
         self.assertTrue(p.__class__.__name__ == 'ParallelGen')
-        self.assertEquals(list(p.flatmap(double)), map(double, chain(*[range(10),range(10)])))
+        self.assertEquals(list(p.flatmap(double_iterables)), map(double, chain(*[range(10),range(10)])))
         self.assertFalse(p.flatmap(double) is p)
-    
+
     def test_chaining(self):
-        p = parallel((d for d in [range(10),range(10)]))
+        p = parallel((d for d in chain(range(10),range(10))))
         self.assertTrue(p.__class__.__name__ == 'ParallelGen')
-        self.assertEquals(list(p.flatten().map(double)), list(parallel((d for d in [range(10),range(10)])).flatmap(double)))
-    
+        self.assertEquals(list(p.filter(is_even).map(double)), list(parallel([range(10),range(10)]).flatmap(double_evens_in_iterables)))
+
     def test_reduce(self):
         p = parallel((d for d in ['a', 'a', 'b']))
         self.assertTrue(p.__class__.__name__ == 'ParallelGen')
         reduced = p.reduce(group_letters, defaultdict(list))
         self.assertEquals(dict(a=['a','a'], b=['b',]), dict(reduced))
         self.assertFalse(reduced is p)
-    
-    def test_gen_func_flatten(self):
-        def inner_gen():
-            for d in [range(10),range(10)]:
-                yield d
-        p = parallel(inner_gen)
-        self.assertTrue(p.__class__.__name__ == 'ParallelGen')
-        self.assertEquals(list(p.flatten()), list(chain(*[range(10),range(10)])))
-        
+
     def test_gen_func_foreach(self):
         def inner_gen():
             for d in [range(10),range(10)]:
@@ -120,7 +107,7 @@ class TestGen(unittest.TestCase):
         p.foreach(double)
         self.assertEquals(list(p), map(double, [range(10),range(10)]))
         self.assertTrue(p.foreach(double) is None)
-        
+
     def test_gen_func_map(self):
         def inner_gen():
             for d in [range(10),range(10)]:
@@ -130,7 +117,7 @@ class TestGen(unittest.TestCase):
         mapped = p.map(double)
         self.assertEquals(list(mapped), map(double, [range(10),range(10)]))
         self.assertFalse(mapped is p)
-    
+
     def test_gen_func_filter(self):
         def inner_gen():
             for d in ['a','2','3']:
@@ -141,24 +128,24 @@ class TestGen(unittest.TestCase):
         filtered = p.filter(pred)
         self.assertEquals(list(filtered), list(['2','3']))
         self.assertFalse(filtered is p)
-        
+
     def test_gen_func_flatmap(self):
         def inner_gen():
             for d in [range(10),range(10)]:
                 yield d
         p = parallel(inner_gen)
         self.assertTrue(p.__class__.__name__ == 'ParallelGen')
-        self.assertEquals(list(p.flatmap(double)), map(double, chain(*[range(10),range(10)])))
+        self.assertEquals(list(p.flatmap(double_iterables)), map(double, chain(*[range(10),range(10)])))
         self.assertFalse(p.flatmap(double) is p)
-    
+
     def test_gen_func_chaining(self):
         def inner_gen():
-            for d in [range(10),range(10)]:
+            for d in chain(*[range(10),range(10)]):
                 yield d
         p = parallel(inner_gen)
         self.assertTrue(p.__class__.__name__ == 'ParallelGen')
-        self.assertEquals(list(p.flatten().map(double)), list(parallel((d for d in [range(10),range(10)])).flatmap(double)))
-    
+        self.assertEquals(list(p.filter(is_even).map(double)), list(parallel([range(10),range(10)]).flatmap(double_evens_in_iterables)))
+
     def test_gen_func_reduce(self):
         def inner_gen():
             for d in ['a', 'a', 'b']:
@@ -168,10 +155,10 @@ class TestGen(unittest.TestCase):
         reduced = p.reduce(group_letters, defaultdict(list))
         self.assertEquals(dict(a=['a','a'], b=['b',]), dict(reduced))
         self.assertFalse(reduced is p)
-                
+
 
 class TestFactories(unittest.TestCase):
-    
+
     def test_returns_gen(self):
         p = parallel((d for d in [range(10),range(10)]))
         self.assertTrue(p.__class__.__name__ == 'ParallelGen')
@@ -186,11 +173,11 @@ class TestFactories(unittest.TestCase):
         self.assertTrue(p.__class__.__name__ == 'ParallelGen')
         p = parallel(set())
         self.assertTrue(p.__class__.__name__ == 'ParallelGen')
-    
+
     def test_lazy_parallel(self):
         with self.assertRaises(DeprecationWarning):
             p = lazy_parallel((d for d in [range(10),range(10)]))
-    
+
     def test_class_depr_warning(self):
         with self.assertRaises(DeprecationWarning):
             p = ParallelSeq((d for d in [range(10),range(10)]))
@@ -200,7 +187,7 @@ class TestFactories(unittest.TestCase):
             p = ParallelDict((d for d in [range(10),range(10)]))
         with self.assertRaises(DeprecationWarning):
             p = ParallelString((d for d in [range(10),range(10)]))
-    
+
     def test_raises_exception(self):
         def inner_func():
             return 1
@@ -212,14 +199,14 @@ class TestFactories(unittest.TestCase):
             p = parallel(1)
         with self.assertRaises(TypeError):
             p = parallel(InnerClass())
-        
-    
+
+
 def _print(item):
-    print item 
-    return item       
- 
+    print item
+    return item
+
 def to_upper(item):
-    return item.upper()   
+    return item.upper()
 
 def is_digit(item):
     return item.isdigit()
@@ -228,8 +215,8 @@ def is_digit_dict(item):
     return item[1].isdigit()
 
 def add_up(x,y):
-    return x+y   
-    
+    return x+y
+
 def group_letters(all, letter):
     all[letter].append(letter)
     return all
@@ -239,9 +226,30 @@ def group_letters_dict(all, letter):
     all[letter].append(letter)
     return all
 
+def chain_iterables(its):
+    return chain(*its)
+
 def double(item):
-    return item * 2 
-    
+    return item * 2
+
+def double_iterables(l):
+    for item in l:
+        return map(double, l)
+
+def is_even(i):
+    if i & 1:
+        return False
+    else:
+        return True
+
+def double_evens(i):
+    if is_even(i):
+        return i * 2
+
+def double_evens_in_iterables(l):
+    for item in l:
+        return map(double, filter(is_even, l))
+
 def double_dict(item):
     k,v = item
     try:

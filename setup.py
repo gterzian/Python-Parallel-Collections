7d501453d52a07edad87e7614f573a58ce88e718
Gregory <gterzian@users.noreply.github.com>
2016-10-22 16:39:50 +0800

    bump to version 2.0.0

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import parallel
 setup(
     name='python-parallel-collections',
     data_files=[('', ['requirements.txt', 'README.md', '.gitignore']),],
-    version='1.2.1',
+    version='2.0.0',
     packages=['parallel',],
     description='parallel support for map/reduce style operations',
     long_description='''
-    
+
     This package provides a convenient interface to perform map/filter/reduce style operation on standard Python data structures and generators in multiple processes.
     The parallelism is achieved using the Python 2.7 backport of the concurrent.futures package.
-    If you can define your problem in terms of map/reduce/filter operations, it will run on several parallel Python processes on your machine, taking advantage of multiple cores. 
+    If you can define your problem in terms of map/reduce/filter operations, it will run on several parallel Python processes on your machine, taking advantage of multiple cores.
     \n
     Examples at https://github.com/gterzian/Python-Parallel-Collections
     Feedback and contributions highly sought after!''',
